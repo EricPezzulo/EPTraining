b1af7dd66c4e16bfd3c50c07d6a17239a3b88629
Eric Pezzulo <eric.pezzulo@gmail.com>
2024-06-10 19:59:16 -0400

    updateded /clients/[clients]/page.tsx file to use a image placeholder if the user does not have one.

--- src/app/clients/[client]/page.tsx
+++ src/app/clients/[client]/page.tsx
@@ -140,11 +140,6 @@ export default function ClientPage() {
     };
     fetchData(clientId);
   }, [clientId]);
-// console.log(user)
-  // useEffect(() => {
-  //   const foundUser = users.find((user) => user.clientId === id) || null;
-  //   if (foundUser) setUser(foundUser);
-  // }, [user, id]);
 
   async function postRequest() {
     const res = fetch("http://localhost:3000/clients/api", {
@@ -153,68 +148,12 @@ export default function ClientPage() {
       body: JSON.stringify(sendData),
     });
   }
-
-  // console.log(clientData);
   return (
     user && (
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <Sidebar />
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-            {/* <Sheet>
-              <SheetTrigger asChild>
-                <Button size="icon" variant="outline" className="sm:hidden">
-                  <PanelLeft className="h-5 w-5" />
-                  <span className="sr-only">Toggle Menu</span>
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="left" className="sm:max-w-xs">
-                <nav className="grid gap-6 text-lg font-medium">
-                  <Link
-                    href="#"
-                    className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-                  >
-                    <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-                    <span className="sr-only">Acme Inc</span>
-                  </Link>
-                  <Link
-                    href="#"
-                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                  >
-                    <Home className="h-5 w-5" />
-                    Dashboard
-                  </Link>
-                  <Link
-                    href="#"
-                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                  >
-                    <ShoppingCart className="h-5 w-5" />
-                    Orders
-                  </Link>
-                  <Link
-                    href="#"
-                    className="flex items-center gap-4 px-2.5 text-foreground"
-                  >
-                    <Package className="h-5 w-5" />
-                    Products
-                  </Link>
-                  <Link
-                    href="#"
-                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                  >
-                    <Users2 className="h-5 w-5" />
-                    Customers
-                  </Link>
-                  <Link
-                    href="#"
-                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                  >
-                    <LineChart className="h-5 w-5" />
-                    Settings
-                  </Link>
-                </nav>
-              </SheetContent>
-            </Sheet> */}
             <Breadcrumb className="hidden md:flex">
               <BreadcrumbList>
                 <BreadcrumbItem>
@@ -250,7 +189,7 @@ export default function ClientPage() {
                   className="overflow-hidden rounded-full"
                 >
                   <Image
-                    src={user?.clientPicture}
+                    src={user?.clientPicture|| '/images/Ichigo.jpeg'}
                     width={36}
                     height={36}
                     alt="Avatar"
@@ -594,15 +533,15 @@ export default function ClientPage() {
                       </Button>
                     </CardContent>
                   </Card>
-                  <Card x-chunk="dashboard-07-chunk-5">
+                  {/* <Card x-chunk="dashboard-07-chunk-5">
                     <CardHeader>
-                      <CardTitle>{clientData?.name}</CardTitle>
+                      <CardTitle>{user?.name}</CardTitle>
                       <CardDescription>
                         {clientData?.company.catchPhrase}
                       </CardDescription>
                     </CardHeader>
                     <CardContent>
-                      {/*  */}
+
                       <Input
                         onChange={(e) => setSendData(e.target.value)}
                         type="text"
@@ -616,7 +555,7 @@ export default function ClientPage() {
                         Send to API
                       </Button>
                     </CardContent>
-                  </Card>
+                  </Card> */}
                 </div>
               </div>
               <div className="flex items-center justify-center gap-2 md:hidden">
